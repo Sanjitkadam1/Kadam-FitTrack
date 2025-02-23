650db52807297d4f7ac62ad2fb1789982a0a2479
Sanjit Kadam <164111429+Sanjitkadam1@users.noreply.github.com>
2025-02-22 23:54:48 -0600

    Finished main page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
- package com.example.myapplication
+package com.example.myapplication
+
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.LayoutRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,14 +16,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import fittrack.app.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContentView(R.layout.layout)
 
-        }
+        val editButton: ImageButton = findViewById(R.id.editButton)
+        editButton.setImageResource(R.drawable.edit)
+
+        val pagesButton: ImageButton = findViewById(R.id.PagesButton)
+        pagesButton.setImageResource(R.drawable.pages_button)
+
+        val stepsView: ImageView = findViewById(R.id.stepsImage)
+        stepsView.setImageResource(R.drawable.shoeprints)
+
+        val distView: ImageView = findViewById(R.id.distImage)
+        distView.setImageResource(R.drawable.running)
+
+        val calView: ImageView = findViewById(R.id.calImage)
+        calView.setImageResource(R.drawable.flame_1)
     }
 }
 
